5f8334b854a23498cad8fe2f02ed7afce1d40dff
FloFlo <florian.dirnberger@tum.de>
2024-06-12 00:05:10 +0200

    Hand tracking
    hand tracking works for point model of hand, ankles are points

---
+++ Assets/Models/Scripts/HandTracking.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = udpReceive.data;
+
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+        print(data);
+        string[] points = data.Split(',');
+        print(handPoints[0]);
+
+        for ( int i = 0; i < 21; i++)
+        {
+            float x = float.Parse(points[i * 3]) / 100; //decide on a suitable factor 
+            float y = float.Parse(points[i * 3 + 1]) / 100;
+            float z = float.Parse(points[i * 3 + 2]) / 100;
+
+            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+    }
+}
