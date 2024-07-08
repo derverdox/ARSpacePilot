b6dc27d6fb6a2023196bd4dcde735c5df1e62318
derverdox <mail.ysp@web.de>
2024-07-08 13:48:24 +0200

    Adding the ability to change the scene to load from main menu

--- Assets/Models/Scripts/Menu/Menu.cs
+++ Assets/Models/Scripts/Menu/Menu.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public SceneAsset sceneToLoad;
     private void Update()
     {
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene("Level1");
+            SceneManager.LoadScene(sceneToLoad.name);
             SceneManager.UnloadSceneAsync("Menu");
         }
     }
